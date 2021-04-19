6c370f3db76ac34e6354357c1df69e2e8fd521dc
karaage0703 <coldchicken@gmail.com>
2021-04-19 22:48:05 +0900

    Refactoring template and separate playing music file from original file

--- recur_div_square.py
+++ recur_div_square.py
@@ -1,6 +1,5 @@
 import pyxel
 import template
-import time
 
 
 def div_rect(x_pos, y_pos, wd, ratio, color, thr):
@@ -51,13 +50,10 @@ class Euclid(template.Base):
         self.num_b = 6
         self.color = 1
         self.thr = 10
-        self.music_x = 0
-        self.music_y = 0
-        self.music_size = 20
-        self.music_flag = True
-        self.music_timer = 0
-        self.music_wait = 1
+
         super().__init__()
+        pyxel.init(self.window_width, self.window_height, caption="Template")
+        pyxel.run(self.update, self.draw)
 
     def update(self):
         super().update()
@@ -74,23 +70,6 @@ class Euclid(template.Base):
         if self.num_a is not self.num_b:
             ratio = self.num_b / self.num_a
         div_square(0, 0, self.window_width, ratio, self.color, self.thr)
-
-        if self.music_flag is True:
-            self.music_flag = False
-            self.music_timer = time.time()
-            self.music_x += self.music_size
-            while self.music_x > self.window_width:
-                self.music_x -= self.window_width
-                self.music_y += self.music_size
-            while self.music_y > self.window_height:
-                self.music_y -= self.window_height
-            note = pyxel.pget(self.music_x, self.music_y) % 3
-            pyxel.play(0, note, loop=False)
-
-        else:
-            if time.time() - self.music_timer > self.music_wait:
-                self.music_flag = True
-        pyxel.rect(self.music_x, self.music_y, self.music_size, self.music_size, 1) 
 
 
 if __name__ == '__main__':
--- template.py
+++ template.py
@@ -4,23 +4,20 @@ import pygame.midi
 
 class Base:
     def __init__(self):
-        pygame.midi.init()
-        self.midi_events = None
-        self.midi_input = None
+        # general setting
         self.window_width = 256
         self.window_height = 256
 
+        # midi setting
+        self.midi_events = None
+        self.midi_input = None
+
+        pygame.midi.init()
         for i in range(pygame.midi.get_count()):
             interf, name, input_dev, output_dev, opened = pygame.midi.get_device_info(i)
             if input_dev and b'X-TOUCH MINI' in name:
                 print('X-TOUCH MINI is found, midi id=' + str(i))
                 self.midi_input = pygame.midi.Input(i)
-
-        pyxel.init(self.window_width, self.window_height, caption="Template")
-        pyxel.sound(0).set("c3", "p", "7", "s", 5)
-        pyxel.sound(1).set("d3", "p", "7", "s", 5)
-        pyxel.sound(2).set("e3", "p", "7", "s", 5)
-        pyxel.run(self.update, self.draw)
 
     def update(self):
         if self.midi_input is not None:
@@ -33,9 +30,14 @@ class Base:
             pyxel.quit()
 
     def draw(self):
-        pyxel.cls(0)
+        pyxel.cls(1)
 
 
 if __name__ == '__main__':
-    print("base")
-    Base()
+    class Blank(Base):
+        def __init__(self):
+            super().__init__()
+            pyxel.init(self.window_width, self.window_height, caption="Template")
+            pyxel.run(self.update, self.draw)
+
+    Blank()
