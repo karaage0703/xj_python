89cb00f86dce07fda4efce96c9606373fd8c285d
karaage0703 <coldchicken@gmail.com>
2021-07-25 12:35:40 +0900

    Modify for pip install error

--- setup.py
+++ setup.py
@@ -1,17 +1,13 @@
 from setuptools import setup, find_packages
 
 
-def _requires_from_file(filename):
-    return open(filename).read().splitlines()
-
-
 setup(
     name='xj_python',
-    version='0.1',
+    version='0.11',
     license='MIT',
     description='xj system',
     author='karaage0703',
     url='https://github.com/karaage0703/xj_python',
     packages=find_packages(),
-    install_requires=_requires_from_file('requirements.txt')
+    install_requires=['pygame', 'pyxel']
 )
